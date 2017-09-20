8bf0cbb552b00ff60224d669e0f151e66281a5d6
asingh67 <asingh67@users.noreply.github.com>
2017-09-20 16:33:02 +0530

    Added webpack config Moved App to its own component and imported the component in index.js Build the bundle.js with webpack and 1st successful test of React

--- react-isomorphic/client/index.js
+++ react-isomorphic/client/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-export default class App extends React.Component{
-    render(){
-        return <div>Hello World from React</div>;
-    }
-}
+import App from './components/app';
+
 ReactDOM.render(<App/>, document.getElementById('app'));
